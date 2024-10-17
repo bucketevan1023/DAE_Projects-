3a6b55a0d0b578dfb89333df2a599529d4316d16
bucketevan1023 <bucketevan1023@gmail.com>
2024-10-17 18:06:43 -0400

    i am adding my figma file comtaining

---
+++ python_1/gloves.py
@@ -0,0 +1,23 @@
+boxing_gloves1= "press anywhere to continue"
+
+
+print(boxing_gloves1)
+
+guess_the_number= 61-100000
+
+print(guess_the_number)  
+
+Dogs_Vs_cats= 100+100-110-100
+
+print(Dogs_Vs_cats)
+
+Float=1.0+6.7-8.0
+
+print(Float)
+
+B_value=True   
+
+print(B_value)
+
+
+
