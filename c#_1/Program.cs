423aeb0c077dd4879dfcc8b960c5516fde7e6e7b
bucketevan1023 <bucketevan1023@gmail.com>
2025-02-04 18:19:41 -0500

    marvel rivals 2

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 
+class SecretBoxingMatch // Class demonstrating access levels
+{
+    // Private field (can only be accessed within this class)
+    private string matchDetails = "Top secret boxing match";
+
+    // Public method (can be accessed outside this class)
+    public void ShowMatchDetails()
+    {
+        Console.WriteLine("Accessing Private Data: " + matchDetails);
+    }
+}
+
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Hello, World!");
 
-        // Variables with different data types
+        // Demonstrating Variables with Data Types
         int myVariable = 12;
         bool isGameActive = false;
         double myDouble = 3.15;
-        
-        // Access Level Example (private variable inside a class)
-        string secretBoxingMatch = "Top secret boxing match";
 
-        // Array/List Example
+        //  Demonstrating List (Array)
         List<string> enemies = new List<string>();
         enemies.Add("Witch");
         enemies.Add("Swordsman");
         enemies.Add("Goblin");
 
         // Loop through the list
+        Console.WriteLine("Enemy List:");
         foreach (string enemy in enemies)
         {
-            Console.WriteLine("Enemy: " + enemy);
+            Console.WriteLine("- " + enemy);
         }
 
-        // Calling the function
+        // Calling a Function
         int sum = Add(6, 4);
         Console.WriteLine("Sum: " + sum);
+
+        // Demonstrating Access Levels
+        SecretBoxingMatch match = new SecretBoxingMatch();
+        match.ShowMatchDetails(); // Accessing private data through a public method
     }
 
-    // Function Example
+    //  Function Example
     static int Add(int a, int b)
     {
         return a + b;
--- c#_1/boxx.cs
+++ c#_1/boxx.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class Boxx : MonoBehaviour
 {
-    // Variables with different data types
-    int number = 5;
-    string king = "Hello World";
+    //  Variables with different data types
+    private int number = 5; // Example of a private field
+    public string king = "Hello World"; // Example of a public field
 
-    // Start() is called before the first frame update
+    // Start() is called once before the first frame update
     void Start()
     {
         Debug.Log("Hello, World");
 
-        // Conditional Statement
+        //  Conditional Statement
         if (number > 134)
         {
             Debug.Log("The number is bigger than 134");
@@ -21,7 +21,7 @@ public class Boxx : MonoBehaviour
             Debug.Log("The number is smaller than 134");
         }
 
-        // Loop Example
+        //  Loop Example
         for (int i = 1; i <= 5; i++)
         {
             Debug.Log("Number: " + i);
