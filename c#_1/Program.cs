eb65522dc394abe4504b5bdae3a2b5b52c4685a6
bucketevan1023 <bucketevan1023@gmail.com>
2025-02-04 17:29:22 -0500

    adding new stuff

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -3,3 +3,11 @@ Console.WriteLine("Hello, World!");
 int myVariable = 12
 bool isGameActive = False 
 double MyDouble = 3.15 
+private string secretboxingmatch = "top secret boxingmatch ";
+list <string> enemies = new list<string>();
+enemies.add("witch");
+enemies.add ("swordsman");
+enemies.add("goblin");
+
+Int Add(int 6, int 4);
+return 6 + 4;
